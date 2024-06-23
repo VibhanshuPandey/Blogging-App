658cdabcf9078c8f24a61971565c91245eb3131d
VibhanshuPandey <pandeyvibhanshu007@gmail.com>
2024-06-23 21:23:26 +0530

    added components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div classname={styles.container}>
+    <Featured/>
+    <CategoryList/>
+    <div classname={styles.content}>
+      <CardList/>
+      <Menu/>
+    </div>
+    
+  </div>;
 }
