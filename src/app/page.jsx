4e9d17a68cd7a2208c34fb965e8e7597dcd93609
VibhanshuPandey <pandeyvibhanshu007@gmail.com>
2024-06-23 22:46:34 +0530

    Added menu

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,17 @@ import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
 
-export default function Home() {
-  return <div classname={styles.container}>
-    <Featured/>
-    <CategoryList/>
-    <div classname={styles.content}>
-      <CardList/>
-      <Menu/>
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList page={page}/>
+        <Menu />
+      </div>
     </div>
-    
-  </div>;
+  );
 }
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,27 @@
-import react from 'react'
-import styles from "./featured.module.css"
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-    return (
-        <div className={styles.container}>Featured</div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey MNV here!</b> Discover our stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>The Future is Now: Upcoming Technologies to Watch in 2024</h1>
+          <p className={styles.postDesc}>
+          The world of technology is rapidly evolving, and as we move into 2024, several emerging technologies are set to transform our lives. Artificial intelligence and machine learning are becoming increasingly integrated into healthcare, finance, and retail, offering improved diagnostics, personalized experiences, and enhanced security. Quantum computing is nearing practical application, promising breakthroughs in drug discovery, cryptography, and optimization. The rollout of 5G will enable smart cities, autonomous vehicles, and expanded IoT networks. Extended Reality (XR) will revolutionize education, entertainment, and remote work with immersive experiences. Biotechnology advances in gene editing and personalized medicine will redefine healthcare, while clean energy technologies like advanced solar power and hydrogen fuel will drive sustainability. Robotics and automation will enhance manufacturing, healthcare, and service industries. Blockchain will bring transparency to supply chains, decentralized finance, and secure digital identities. Space exploration is set to expand with commercial space travel, enhanced satellite technology, and the potential for space mining. Embracing these innovations will be key to leveraging their potential for personal and professional growth.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
